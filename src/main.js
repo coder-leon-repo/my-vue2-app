13b472fee8f2555f7438a266288c2d8991cac729
Leon <geek_leon@outlook.com>
2024-07-25 15:37:01 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
 
-Vue.config.productionTip = false;
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  el: '#app',
+  render: h => h(App)
+})
